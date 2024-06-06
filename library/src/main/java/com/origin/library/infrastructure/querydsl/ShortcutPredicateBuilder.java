a6771987287a3992cc88021cd4904c23ef053828
hku47w <168148058+hku47w@users.noreply.github.com>
2024-06-07 06:16:55 +0800

    feat(query): add andAnyOf and orAllOf methods to ShortcutPredicateBuilder for library

--- library/src/main/java/com/origin/library/infrastructure/querydsl/ShortcutPredicateBuilder.java
+++ library/src/main/java/com/origin/library/infrastructure/querydsl/ShortcutPredicateBuilder.java
@@ -1,6 +1,7 @@
 package com.origin.library.infrastructure.querydsl;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Predicate;
 
 public class ShortcutPredicateBuilder {
@@ -13,8 +14,52 @@ public class ShortcutPredicateBuilder {
 		return this;
 	}
 
+	public ShortcutPredicateBuilder and(boolean condition, Predicate... b) {
+		if (condition) {
+			return and(b);
+		}
+		return this;
+	}
+
+	public ShortcutPredicateBuilder andAnyOf(boolean condition, Predicate... b) {
+		if (condition) {
+			return addAnyOf(b);
+		}
+		return this;
+	}
+
 	public ShortcutPredicateBuilder and(Predicate b) {
 		q.and(b);
+		return this;
+	}
+
+	/**
+	 * Create the intersection of this and the union of the given args
+	 * {@code (this && (arg1 && arg2 ... && argN))}
+	 *
+	 * @param b union of predicates
+	 * @return the current object
+	 */
+	public ShortcutPredicateBuilder and(Predicate... b) {
+		if (b.length == 0) {
+			return this;
+		}
+		q.and(ExpressionUtils.allOf(b));
+		return this;
+	}
+
+	/**
+	 * Create the intersection of this and the union of the given args
+	 * {@code (this && (arg1 || arg2 ... || argN))}
+	 *
+	 * @param b union of predicates
+	 * @return the current object
+	 */
+	public ShortcutPredicateBuilder addAnyOf(Predicate... b) {
+		if (b.length == 0) {
+			return this;
+		}
+		q.andAnyOf(b);
 		return this;
 	}
 
@@ -25,8 +70,52 @@ public class ShortcutPredicateBuilder {
 		return this;
 	}
 
+	public ShortcutPredicateBuilder or(boolean condition, Predicate... b) {
+		if (condition) {
+			return or(b);
+		}
+		return this;
+	}
+
+	public ShortcutPredicateBuilder orAllOf(boolean condition, Predicate... b) {
+		if (condition) {
+			return orAllOf(b);
+		}
+		return this;
+	}
+
 	public ShortcutPredicateBuilder or(Predicate b) {
 		q.or(b);
+		return this;
+	}
+
+	/**
+	 * Create the union of this and the intersection of the given args
+	 * {@code (this || (arg1 || arg2 ... || argN))}
+	 *
+	 * @param b intersection of predicates
+	 * @return the current object
+	 */
+	public ShortcutPredicateBuilder or(Predicate... b) {
+		if (b.length == 0) {
+			return this;
+		}
+		q.or(ExpressionUtils.anyOf(b));
+		return this;
+	}
+
+	/**
+	 * Create the union of this and the intersection of the given args
+	 * {@code (this || (arg1 && arg2 ... && argN))}
+	 *
+	 * @param b intersection of predicates
+	 * @return the current object
+	 */
+	public ShortcutPredicateBuilder orAllOf(Predicate... b) {
+		if (b.length == 0) {
+			return this;
+		}
+		q.or(new BooleanBuilder().orAllOf(b));
 		return this;
 	}
 
--- library/src/main/java/com/origin/library/infrastructure/repository/UserRepository.java
+++ library/src/main/java/com/origin/library/infrastructure/repository/UserRepository.java
@@ -2,10 +2,59 @@ package com.origin.library.infrastructure.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+import com.origin.library.domain.Page;
+import com.origin.library.domain.QUser;
 import com.origin.library.domain.User;
+import com.origin.library.infrastructure.querydsl.ShortcutPagingQuery;
+import com.origin.library.infrastructure.querydsl.ShortcutPredicateExecutor;
+import com.querydsl.core.types.Predicate;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository
+		extends JpaRepository<User, Long>, ShortcutPredicateExecutor<User>, AdvancedUserRepository {
 	Optional<User> findByUsername(String username);
+
+	// FIXME: remove the example
+	default Page<User> searchUsers(String keyword, int pageNumber, int pageSize) {
+		QUser a = QUser.user;
+		Predicate p = predicate()
+				.and(isNotBlank(keyword), a.username.like(quoteLike(keyword)))
+				.build();
+		Sort sort = asc("id");
+		return findAll(p, sort, pageNumber, pageSize);
+	}
+}
+
+interface AdvancedUserRepository {
+	Page<User> searchUsers(String keyword, int pageSize);
+}
+
+@Service
+class AdvancedUserRepositoryImpl extends ShortcutPagingQuery implements AdvancedUserRepository {
+	// FIXME: remove the example
+	@Override
+	public Page<User> searchUsers(String keyword, int pageSize) {
+		QUser a = QUser.user;
+		// where username like '%user%'
+		// and id>0 and (id>0 and id<1000) and (id>-1 or id<1001)
+		// or id<0 or (id<-1 or id<-2) or (id<-3 and id<-4)
+		Predicate p = predicate()
+				.and(isNotBlank(keyword), a.username.like(quoteLike(keyword)))
+				.and(a.id.gt(0))
+				.and(a.id.gt(0), a.id.lt(1000))
+				.andAnyOf(true, a.id.gt(-1), a.id.lt(1001))
+				.or(a.id.lt(0))
+				.or(a.id.lt(-1), a.id.lt(-2))
+				.orAllOf(true, a.id.lt(-3), a.id.lt(-4))
+				.build();
+		Page<User> r = findAll(
+				q -> q.select(a)
+						.from(a)
+						.where(p),
+				pageSize);
+		return r;
+	}
 }
