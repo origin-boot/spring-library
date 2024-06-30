601aafa5ffdec3b69f030cc471221164f18cae05
hku47w <168148058+hku47w@users.noreply.github.com>
2024-06-30 12:57:08 +0800

    chore(lint): run Java code detection script to CI

--- javalint.sh
+++ javalint.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-CS_JAR="checkstyle-10.17.0-all.jar"
+CS_VERSION="10.17.0"
+CS_JAR="checkstyle-${CS_VERSION}-all.jar"
+CS_CONFIG="google_checks.xml"
+
+if [ ! -f "$CS_CONFIG" ]; then
+    echo "Checked that ${CS_CONFIG} does not exist and is downloading..."
+    wget -q https://raw.githubusercontent.com/checkstyle/checkstyle/checkstyle-"$CS_VERSION"/src/main/resources/"$CS_CONFIG"
+    if [ $? -ne 0 ]; then
+        echo "Download failed, please check whether the network connection or URL is correct."
+        exit 1
+    fi
+fi
+
 
 if [ ! -f "$CS_JAR" ]; then
     echo "Checked that ${CS_JAR} does not exist and is downloading..."
-    wget -q https://github.com/checkstyle/checkstyle/releases/download/checkstyle-10.17.0/"$CS_JAR"
+    wget -q https://github.com/checkstyle/checkstyle/releases/download/checkstyle-"$CS_VERSION"/"$CS_JAR"
     if [ $? -ne 0 ]; then
         echo "Download failed, please check whether the network connection or URL is correct."
         exit 1
@@ -13,10 +25,9 @@ fi
 
 params=("$@")
 
-# 检查是否有参数传递
 if [ ${#params[@]} -eq 0 ]; then
     echo "No parameters are provided, please specify the Java source file to be checked."
     exit 1
 fi
 
-java -jar $CS_JAR -c /google_checks.xml "${params[@]}"
+java -jar $CS_JAR -c $CS_CONFIG "${params[@]}"
